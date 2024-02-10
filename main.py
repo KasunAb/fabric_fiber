2b38d2d0dbdb0904ba4390869b21648a8cc4a09a
Kasun Abegunawardhana <75435489+KasunAb@users.noreply.github.com>
2024-02-10 16:30:57 +0530

    Update app.run() configuration

--- main.py
+++ main.py
@@ -73,6 +73,8 @@ def predict_model2():
         return jsonify(predictions)
 
 
+# if __name__ == '__main__':
+#     app.run(debug=True)
+    
 if __name__ == '__main__':
-    app.run(debug=True)
-
+    app.run(host='0.0.0.0', port='8080')
